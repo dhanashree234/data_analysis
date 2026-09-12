f0c42131d4c435d8a994a390126cb6b59571211e
dhanashree234 <dhanashree.mahajan234@gmail.com>
2026-09-12 14:40:56 +0100

    python scripts modified

--- python/python_programs 1/rectangle_area.py
+++
@@ -1,8 +0,0 @@
-# 3)Write a Python program to store the length and breadth of a rectangle and calculate its area. Pov: area = length*breadth
-
-length = 3
-breadth = 5
-
-area = length.__mul__(breadth)
-
-print(f"Area of the rectangle with length {length} and breadth {breadth} = {area}")
--- python/python_programs 1/sum.py
+++
@@ -1,6 +0,0 @@
-
-# 2)Write a Python program to store two integers and find their sum.
-num1 = 5
-num2 = 2
-
-print(f"Sum of {num1} and {num2} = ", num1.__add__(num2))
